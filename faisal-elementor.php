980a60e0d0ad0f1a3fad669c9a9f99c176d19a8e
faisal46 <faisalamirmostafa@gmail.com>
2022-03-14 22:42:49 +0600

    first commit

---
+++ faisal-elementor.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Faisal Elementor Addon
+ * Description: Faisal Elementor Addon plugin for extend functions
+ * Plugin URI:  http://tenpixelui.com/
+ * Version:     1.0.0
+ * Author:      Faisal
+ * Author URI:  http://faisal.rajtechbd.com/
+ * Text Domain: fea
+ */
