e38baf067622acaf9725c0d020e95c8812446cb1
faisal46 <faisalamirmostafa@gmail.com>
2022-03-14 22:50:33 +0600

    name change

--- faisal-elementor.php
+++ faisal-elementor.php
@@ -1,10 +1,13 @@
 <?php
 /**
  * Plugin Name: Faisal Elementor Extend
- * Description: Faisal Elementor Addon plugin for extend functions
+ * Description: A plugin are extended the 
  * Plugin URI:  https://github.com/faisal46/wp-elementor-extend-plugin
- * Version:     1.0.0
- * Author:      Faisal
- * Author URI:  http://faisal.rajtechbd.com/
+ * Author: Md. Faisal Amir Mostafa
+ * Author URI: https://www.faisal.rajtechbd.com/
+ * Version: 1.0.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wpfee
+ * Domain Path: /langusges/
  */
